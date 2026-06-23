e2b76521cfadf594ead6a0c7f0055a9ec86340cf
flex58 <164533598+Flex58@users.noreply.github.com>
2026-06-23 11:34:54 +0200

    fix: fix sessionstorage connection

--- config/session.js
+++ config/session.js
@@ -1,15 +1,15 @@
 const expressSession = require("express-session");
 require("dotenv").config();
-const pool = require("../lib/prisma.js");
+const { prisma } = require("../lib/prisma.js");
 const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
 
 module.exports = function () {
   return expressSession({
     secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
+    resave: true,
+    saveUninitialized: true,
     cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, //30days
-    store: new PrismaSessionStore(pool, {
+    store: new PrismaSessionStore(prisma, {
       checkPeriod: 2 * 60 * 1000, //ms
       dbRecordIdIsSessionId: true,
       dbRecordIdFunction: undefined,
