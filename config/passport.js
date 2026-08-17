fc119c77b57228b76d5b6ff832c3c20b1f6f2bd9
flex58 <164533598+Flex58@users.noreply.github.com>
2026-08-17 12:58:01 +0200

    fix: fix critical error in password matching check

--- config/passport.js
+++ config/passport.js
@@ -14,7 +14,7 @@ passport.use(
           return done(null, false, { message: "Incorrect Email or Password" });
         }
 
-        const match = bcrypt.compare(password, user.password);
+        const match = await bcrypt.compare(password, user.password);
 
         if (!match) {
           return done(null, false, {
